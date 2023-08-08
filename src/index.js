bb6cf001276cac9a41eecea77241fa553149db78
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-08-08 16:00:42 +0700

    replate: update code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
