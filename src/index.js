b7067e1355c1d542bc56254d9262583a73269e65
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-08-08 16:14:53 +0700

    sssss

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
